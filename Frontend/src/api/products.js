a204c10d669c0e1109d8d43ba77fc7837ee9704f
rahul <yadavrahul81135@gmail.com>
2026-07-22 19:40:59 +0530

    modifiy api url

--- Frontend/src/api/products.js
+++ Frontend/src/api/products.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "ec2-15-207-254-178.ap-south-1.compute.amazonaws.com";
 
 /**
  * Fetch all products from backend (populated with category ObjectId reference)
