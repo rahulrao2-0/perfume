a4eff3d22ac8d5a6983f84243b4c4168b6456d68
rahul <yadavrahul81135@gmail.com>
2026-07-22 19:58:30 +0530

    update routes and api base url for deployment

--- Backend/Routes/Products.js
+++ Backend/Routes/Products.js
@@ -2,6 +2,6 @@ import express from "express";
 import { Products } from "../Controllers/Products.js";
 const router = express.Router();
 
-router.get("/AllProducts/", Products);
+router.get("/AllProducts", Products);
 
 export default router;
