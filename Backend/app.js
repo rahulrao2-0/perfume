4fd52c5d2fa5434008261ba492773aeee4a6be57
rahul <yadavrahul81135@gmail.com>
2026-07-22 19:49:18 +0530

    update cors to allow requests from vercel app

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+      "https://perfume-seven-drab.vercel.app"
+    ],
     credentials: true,
   })
 );
